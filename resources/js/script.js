6be689472601b6c2e4dbb249b76124eb5ac5d6d2
ThomasSSwanson <67560982+ThomasSSwanson@users.noreply.github.com>
2020-08-27 20:10:35 -0400

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+
+    /* sticky navigation */
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+
+    }, {
+        offset: '60px'
+    });
+
+
+    /* scoll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-signup').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
+
+    /* scroll on navigation */
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+
+    /* animate scrolling */
+      $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInLeft');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__bounce');
+    }, {
+        offset: '50%'
+    });
+    
+    
+});
+
+$('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        //Gets the class name of the element that triggered the event
+        var clicked = element.target.className;
+
+        //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
+            return;
+
+        //Opens and closes the menu
+        if ($(window).width() < 768){
+            nav.slideToggle(200);
+        }
+
+        //Changes icon states of the menu button
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });
